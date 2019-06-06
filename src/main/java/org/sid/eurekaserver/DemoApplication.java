870f99fb65d6ea7d70714d2fe45b9daeb9c6def6
moezboukhari <boukhari.moez@gmail.com>
2019-06-06 11:39:45 +0200

    feat1

--- src/main/java/org/sid/eurekaserver/DemoApplication.java
+++ src/main/java/org/sid/eurekaserver/DemoApplication.java
@@ -1,9 +1,7 @@
 package org.sid.eurekaserver;
 
-import com.github.vanroy.cloud.dashboard.config.EnableCloudDashboard;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
